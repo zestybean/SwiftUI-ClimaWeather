ee7848119987d6d2423fc5f2dc1fd3d84b835198
Brando Lugo <brando.lugo@gmail.com>
2020-12-11 10:29:47 -0600

    Initial Commit

---
+++ Shared/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
